3fa8bc4136ade9b5286d5326eea667d434c8aaab
Ken Ho <ken.ho@hotmail.co.uk>
2016-01-04 18:07:55 +0000

    Added courses index page to show all available courses

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Flixter::Application.routes.draw do
   namespace :instructor do
     resources :courses
   end
+  resources :courses
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
