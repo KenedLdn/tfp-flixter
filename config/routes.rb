eb8825591d93e4938d4e6584a59cad853f18f2a6
Ken Ho <ken.ho@hotmail.co.uk>
2016-01-06 02:38:39 +0000

    Add individual lesson show page

--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,7 @@ Flixter::Application.routes.draw do
     end
   end
   resources :courses
+  resources :lessons, :only => [:show]
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
