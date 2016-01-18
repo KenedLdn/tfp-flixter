895f43175e64101b8d5674ee0d70ba633bb78997
Ken Ho <ken.ho@hotmail.co.uk>
2016-01-18 15:23:24 +0000

    Add Previous Lesson and Next Lesson buttons on lesson page

--- app/models/section.rb
+++ app/models/section.rb
@@ -6,4 +6,14 @@ class Section < ActiveRecord::Base
   ranks :row_order, :with_same => :course_id
 
   validates :title, :presence => true
+
+  def next_section
+    section = course.sections.where("row_order > ?", self.row_order).rank(:row_order).first
+    return section
+  end
+
+  def previous_section
+    section = course.sections.where("row_order < ?", self.row_order).rank(:row_order).last
+    return section
+  end
 end
